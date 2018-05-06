02e63bc383cf4948ca87756ca812c1c9bfa314e0
jake whiteley <odegramusic@gmail.com>
2018-05-06 20:24:21 +0100

    start framework class

--- includes/Framework.php
+++ includes/Framework.php
@@ -5,13 +5,30 @@ namespace Theme;
 use Snap\Core\Hookable;
 
 /**
- * Setup theme.
- *
- * This means registering scripts, sidebars and menus.
- *
- * @since  1.0.0
+ * Adds Bootstrap 4 markup where possible.
  */
 class Framework extends Hookable
 {
+	/**
+	 * Filters to add on init.
+	 * 
+	 * @var array
+	 */
+	public $filters = [
+		'snap_related_pages_widget_defaults' => 'override_related_pages_defaults'
+	];
 
+	/**
+	 * Override the defaults of the related pages widget.
+	 * 
+	 * @param  array $defaults The default arguments.
+	 * @return array
+	 */
+	public function override_related_pages_defaults($defaults)
+	{
+		$defaults['container_start'] = '<ul class="nav flex-column nav-pills" role="navigation">';
+		$defaults['li_class'] = 'nav-item';
+		$defaults['link_class'] = 'nav-link';
+		return $defaults;
+	}
 }
