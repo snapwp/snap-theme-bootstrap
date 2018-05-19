4f95bc13de9fef745e301145b04a19c41c4de0e8
jake whiteley <odegramusic@gmail.com>
2018-05-19 23:28:27 +0100

    add use statements

--- theme/Controllers/TestController.php
+++ theme/Controllers/TestController.php
@@ -3,16 +3,19 @@
 
 namespace Theme\Controllers;
 
+use Snap\Core\Request;
+use Snap\Core\View;
+
 class TestController
 {
     private $view = null;
 
-    public function __construct(\Snap\Core\View $view)
+    public function __construct(View $view)
     {
         $this->view = $view;
     }
 
-    public function index()
+    public function index(Request $request)
     {
         $this->view->render('index');
     }
