2d35ad628caefda2ec5f5f2421decbd956992ab2
jake whiteley <odegramusic@gmail.com>
2018-06-20 23:20:32 +0100

    Tidy

--- functions.php
+++ functions.php
@@ -1,18 +1,4 @@
 <?php
-/*
-*  _______                     ________ ______ 
-* |     __|.-----.---.-.-----.|  |  |  |   __ \
-* |__     ||     |  _  |  _  ||  |  |  |    __/
-* |_______||__|__|___._|   __||________|___|   
-*                      |__|             
-*
-* Version 1.0.0
-*
-* Wordpress theme framework.
-* Bootstrap 4 edition.
-*
-* View our Github: https://github.com/snapwp
-*/
 
 /*
  * Include Snapwp/snap-core and other dependencies,
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+*  _______                     ________ ______ 
+* |     __|.-----.---.-.-----.|  |  |  |   __ \
+* |__     ||     |  _  |  _  ||  |  |  |    __/
+* |_______||__|__|___._|   __||________|___|   
+*                      |__|             
+*
+* Version 1.0.0
+*
+* Wordpress theme framework.
+* Bootstrap 4 edition.
+*
+* View our Github: https://github.com/snapwp
+*/
