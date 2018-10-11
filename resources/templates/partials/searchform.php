4c87f99337235317d7f08957d9695912b8e99a91
Jake Whiteley <jake@craftedbygc.com>
2018-10-11 21:32:04 +0100

    Move id logic out of searchform

--- resources/templates/partials/searchform.php
+++ resources/templates/partials/searchform.php
@@ -1,18 +1,15 @@
 <?php
-// Incase this form is included multiple times, let's create a random ID attr for the label/input
-if ( function_exists( 'random_int' ) ) {
-	$id = 'search_' . random_int( 1000, 8000 );
-} else {
-	$id = uniqid( 'search_', true );
-}
+/*
+ * $searchform_id is always unique to each searchform ensuring the labels target the correct input.
+ */
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<div class="input-group">
-		<label for="<?php echo $id; ?>">
+		<label for="<?php echo $searchform_id; ?>">
 			<span class="sr-only"><?php echo _x( 'Search for:', 'label' ) ?></span>
 		</label>
 		
-		<input type="text" id="<?php echo $id; ?>" class="form-control" placeholder="Search for..." name="s" aria-label="Search for..." required>
+		<input type="text" id="<?php echo $searchform_id; ?>" class="form-control" placeholder="Search for..." name="s" aria-label="Search for..." required>
 		
 		<div class="input-group-append">
 			<button class="btn btn-secondary" type="submit"><?php echo esc_attr_x( 'Search', 'submit button' ); ?></button>
