f38555b57df36b58942c7a97c1ef15fd7b890d01
Jake Whiteley <odegramusic@gmail.com>
2018-11-02 20:45:20 +0000

    Tidy config

--- config/admin.php
+++ config/admin.php
@@ -6,6 +6,8 @@ return [
      *
      * As well as graphical changes, the admin theme also reorders the page menu link to be above posts, and cleans
      * the admin bar.
+     *
+     * @var bool
      */
     'snap_admin_theme' => true,
 
@@ -14,23 +16,31 @@ return [
      * Output custom 'Built using' text in the admin footer.
      *
      * Set to false to output the Snap default text.
+     *
+     * @var false|string
      */
 	'footer_text' => false,
 
 
 	/**
 	 * Whether to display the current WordPress version in the admin footer.
+     *
+     * @var bool
 	 */
 	'show_version' => true,
 
 
 	/**
 	 * Provide a css url to enqueue on the login screen.
+     *
+     * @var false|string
 	 */
 	'login_extra_css' => false,
 
 	/**
 	 * Set the logo link url on the login page.
+     *
+     * @var string
 	 */
 	'login_logo_url' => home_url(),
 ];
