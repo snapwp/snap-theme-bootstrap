26343e9ecf288d1ffc1e3f923f0e52d0547c1b55
Jake Whiteley <odegramusic@gmail.com>
2019-01-10 20:48:31 +0000

    Tidy

--- resources/templates/partials/post-type/post.php
+++ resources/templates/partials/post-type/post.php
@@ -1,27 +1,27 @@
-<?php if ( is_single() ) : ?>
+<?php if (is_single()) : ?>
 
-	<article itemscope itemtype="http://schema.org/Article">
-		<?php the_post_thumbnail( 'post_featured_image', [ 'class' => 'mb-3 img-fluid' ] ); ?>
+    <article itemscope itemtype="http://schema.org/Article">
+        <?php the_post_thumbnail('post_featured_image', ['class' => 'mb-3 img-fluid']); ?>
 
-		<h1 itemprop="name headline"><?php the_title(); ?></h1>
- 		
- 		<div itemprop="articleBody">
-			<?php the_content(); ?>
-		</div>
-	</article>
+        <h1 itemprop="name headline"><?php the_title(); ?></h1>
+
+        <div itemprop="articleBody">
+            <?php the_content(); ?>
+        </div>
+    </article>
 
 <?php else : ?>
 
-	<article <?php post_class( 'card mb-4' ); ?>>
-		<?php the_post_thumbnail( 'post_featured_image', [ 'class' => 'card-img-top img-fluid' ] ); ?>
+    <article <?php post_class('card mb-4'); ?>>
+        <?php the_post_thumbnail('post_featured_image', ['class' => 'card-img-top img-fluid']); ?>
 
-		<div class="card-body">
-			<h4 class="card-title"><?php the_title(); ?></h4>
+        <div class="card-body">
+            <h4 class="card-title"><?php the_title(); ?></h4>
 			
-			<?php the_excerpt(); ?>
+            <?php the_excerpt(); ?>
 
-			<a href="<?php the_permalink(); ?>" class="btn btn-outline-primary">Read more</a>
-		</div>
-	</article>
+            <a href="<?php the_permalink(); ?>" class="btn btn-outline-primary">Read more</a>
+        </div>
+    </article>
 
 <?php endif; ?>
